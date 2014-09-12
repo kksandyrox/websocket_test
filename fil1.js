32e19cae2ac0ec74dc05cfb39b1d17d51b5a6c17
Sandeep Kumar <kksandyrox@gmail.com>
2014-09-12 11:51:38 +0530

    sas

--- fil1.js
+++ fil1.js
@@ -1,3 +1,3 @@
 console.log('Hello world');
-
+console.log('Hello world'
 // %SystemRoot%\system32;%SystemRoot%;%SystemRoot%\system32\Wbem;C:\Ruby200\bin;%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\;C:\Program Files\TortoiseSVN\bin;C:\Program Files (x86)\Zend\ZendServer\bin;C:\Program Files (x86)\Zend\ZendServer\share\ZendFramework\bin;C:\xampp\htdocs\mobilemoney\app\console;C:\Program Files\nodejs\
